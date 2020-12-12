d28bf7cff9b6b6839998c08b88eb28a0122b99b1
Brad Traversy <me@bradtraversy.com>
2020-12-12 08:49:50 +0700

    update readme

---
+++ DataObject.py
@@ -0,0 +1,11 @@
+class Customer:
+    def_init_(seft,CustomterID,CustomerName,ContactName,Adress,City,PostalCode,Country)
+    seft.CustomterID=CustomterID;
+    seft.CustomerName=CustomterName;
+    seft.ContactName=ContactName;
+    seft.Adress=Adress;
+    seft.City=City;
+    seft.PostalCode=PostalCode;
+    seft.Country=Country;
+if _name_ == "_main_"
+   print("this is business object package")
