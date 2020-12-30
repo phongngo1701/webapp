1c1ecbd99c8ab1219638d9a245f6fc2fcd38c469
Brad Traversy <me@bradtraversy.com>
2020-12-30 19:32:20 +0700

    update

---
+++ BACKEND/BusinessObject.py
@@ -0,0 +1,33 @@
+  
+class Customer:
+    def __init__(self, CustomerID=None, CustomerName=None, ContactName=None, Address=None, City=None, PostalCode=None, Country=None):
+        self.CustomerID = CustomerID
+        self.CustomerName = CustomerName
+        self.ContactName = ContactName
+        self.Address = Address
+        self.City = City
+        self.PostalCode = PostalCode
+        self.Country = Country
+
+    def fetch_data(self,data):
+        self.CustomerID = data[0]
+        self.CustomerName = data[1]
+        self.ContactName = data[2]
+        self.Address = data[3]
+        self.City = data[4]
+        self.PostalCode = data[5]
+        self.Country = data[6]
+    
+    def to_json(self):
+        return{
+            'CustomerID' : self.CustomerID,
+            'CustomerName' : self.CustomerName,
+            'ContactName' : self.ContactName,
+            'Address' : self.Address,
+            'City' : self.City,
+            'PostalCode' : self.PostalCode,
+            'Country' : self.Country
+        }
+
+if __name__ == "__main__":
+    print('this is business object package')
--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,30 +1,74 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
-import BusinessObjects as bo
-import DataObject  as do
+import BusinessObject as bo
+import DataObject as do 
+
 
 app = Flask(__name__)
+CORS(app)
 
-db_ip=os.getenv("db_ip") #'10.0.2.15'
-ConnectionData ={}
-ConnectionData ['user'] = 'postgres'
-ConnectionData ['password'] = 'postgres'
-ConnectionData ['host'] = str(db_ip)
-ConnectionData ['port'] = '5432'
-ConnectionData ['database'] = 'northwind'
+db_ip = os.getenv("db_ip")
+ConnectionData = {}
+ConnectionData['user'] = 'postgres'
+ConnectionData['password'] = 'postgres'
+ConnectionData['host'] = str(db_ip)
+ConnectionData['port'] = '5432'
+ConnectionData['database'] = 'northwind'
 
 @app.route("/")
 def hello():
-    c1=bo.Customer(1,'Alfreds Futterkiste','Maria Anders','Obere Str. 57','Berlin','12209','Germany')
-    return c1.CustomerName
-
+   return "hello"
 @app.route("/test_insert")
 def test_insert():
-    #ConnectionString = 'database=northwind user=postgres password =postgres host=10.0.2.15 port=5432'
-    c2=do.Customer(ConnectionData)
-    c1=bo.Customer(1,'Alfreds Futterkiste','Maria Anders','Obere Str. 57','Berlin','12209','Germany')
-    s1=c2.insert(c1)
+    c2 = do.Employee(ConnectionData)
+    c1 = bo.Employee(1,'Thanh','Le','1999', 'Da Nang','VietNam')
+    s1 = c2.insert(c1)
     return s1
 
-if __name__ =="__main__":
+@app.route('/test_send_receive', methods=['POST'])
+def test_send_receive():
+    x = request.json['x']
+    y = x + 1
+    result = {}
+    result['y'] = y
+    return jsonify(result), 200
+
+@app.route('/user/insert', methods=['POST'])
+def user_insert():
+    data = request.json
+    c1 = bo.Customer(data['CustomerID'], data['CustomerName'], data['ContactName'], data['Address'], data['City'], data['PostalCode'], data['Country'])
+    c2 = do.Customer(ConnectionData)
+    s1 = c2.insert(c1)
+    result = {}
+    result['message'] = s1
+    return jsonify(result), 200
+
+@app.route('/user/all')
+def get_all_user():
+    result = do.Customer(ConnectionData).get_all()
+    return jsonify(result), 200
+
+@app.route('/user/get/<int:customer_id>')
+def get_user_by_id(customer_id):
+    c = bo.Customer(CustomerID=customer_id)
+    result = do.Customer(ConnectionData).get_by_id(c)
+    if result[1] != 200:
+        return jsonify({'message':result[0]}), result[1]
+    return jsonify(result[0].to_json()), 200
+
+@app.route('/user/update/<int:customer_id>',methods=['PUT'])
+def update_user_by_id(customer_id):
+    data = request.json
+    c = bo.Customer(CustomerID = customer_id, CustomerName=data['CustomerName'], ContactName=data['ContactName'],Address=data['Address'], City=data['City'], PostalCode=data['PostalCode'], Country=data['Country'])
+    result = do.Customer(ConnectionData).update(c)
+    return jsonify({'message':result[0]}),result[1]
+
+@app.route('/user/delete/<int:customer_id>', methods=['DELETE'])
+def delete_user_by_id(customer_id):
+    c = bo.Customer(CustomerID=customer_id)
+    result = do.Customer(ConnectionData).delete(c)
+    return jsonify({'message':result[0]}), result[1]
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
